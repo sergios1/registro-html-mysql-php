81ebe4dec0ad27c335e41c3361b8b9e40e4cee16
sergio <ssc_daniel201@hotmail.com>
2018-11-20 03:35:06 +0100

    creacion de la interfaz de registro

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title> REGISTRO DE MIEMBROS</TITLE>
+</head>
+
+<body>
+  <h1 align="center">formulario de registro</h1>
+  <form  align="center" method="post" action="" />
+     <table align="center">
+
+       <tr>
+           <td>
+                Nombre:
+           </td>
+
+           <td> 
+              <input type="name" name="nombre"/>//crea el campo para rellenar
+           </td>
+       </tr>
+
+
+       <tr>
+           <td>
+                apellidos:
+           </td>
+
+           <td> 
+              <input type="apellidos" apellidos="apellidos"/>po para rellenar
+           </td>
+       </tr>
+
+
+       <tr>
+           <td>
+                cargo
+           </td>
+
+           <td> 
+              <input type="cargo" cargo="cargo"/>           
+           </td>
+       </tr>
+
+
+
+
+
+   </table >
+//crea botones
+<input type="submit" name="submit" value="guardar"   /><input type="reset"/>
+
+</form>
+</body>
+</html
